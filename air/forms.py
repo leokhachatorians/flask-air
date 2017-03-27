704bf37d14a716e172438699fb9e2ef26e2c1492
leokhachatorians <leokhachatorians@gmail.com>
2017-03-27 16:15:11 -0700

    placeholders to forms

--- air/forms.py
+++ air/forms.py
@@ -1,11 +1,21 @@
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 class NewSheetForm(Form):
-    sheet_name = StringField('Sheet Name', [validators.Length(min=3, max=25), validators.DataRequired()])
-    column_names = StringField('Column Names', [validators.Length(min=3, max=50), validators.DataRequired()])
-    column_types = StringField('Column Types', [validators.Length(min=3, max=50), validators.DataRequired()])
+    sheet_name = StringField('Sheet Name',
+            [validators.Length(min=3, max=25), validators.DataRequired()],
+            render_kw={"placeholder": "People"})
+    column_names = StringField('Column Names',
+            [validators.Length(min=3, max=50), validators.DataRequired()],
+            render_kw={"placeholder": "Name, Age, Location"})
+    column_types = StringField('Column Types',
+            [validators.Length(min=3, max=50), validators.DataRequired()],
+            render_kw={"placeholder": "Text, Text, Text"})
 
 class AddColumnForm(Form):
-    column_name = StringField('Column Name', [validators.Length(min=3, max=25), validators.DataRequired()])
-    column_type = StringField('Column Type', [validators.Length(min=3, max=25), validators.DataRequired()])
+    column_name = StringField('Column Name',
+            [validators.Length(min=3, max=25), validators.DataRequired()],
+            render_kw={"placeholder": "PhoneNumber"})
+    column_type = StringField('Column Type',
+            [validators.Length(min=3, max=25), validators.DataRequired()],
+            render_kw={"placeholder": "Text"})
 
