ae10f6f6fc7a6601f95b17c6e66cdb03353f7b4b
leokhachatorians <leokhachatorians@gmail.com>
2017-03-31 13:06:16 -0700

    removed dead code

--- air/helpers.py
+++ air/helpers.py
@@ -52,80 +52,6 @@ def standardize_form_data(form):
 
     return data
 
-def structure_alter_table_data(action, obj, **kwargs):
-    """
-    Given a DB object, create and structure data
-    which will then be used to generate the SQL
-    commands for alter the user's table.
-    """
-    if action == 'add_col':
-        data = {
-            'table_name': "table_{}".format(obj.id),
-             'new_col': {
-                'name': "col_{}".format(obj.column_num),
-                'type': obj.column_type
-            }
-        }
-    elif action == 'remove_col':
-        data = {
-            'table_name': "table_{}".format(obj.sheet_id),
-            'remove_col': "col_{}".format(obj.column_num)
-        }
-
-    elif action == 'rename_col':
-        data = {
-            'table_name': "table_{}".format(obj.sheet_id),
-            'rename_col': {
-                'old_name': "col_{}".format(obj.column_num),
-                'new_name': "col_{}".format(kwargs['new_col_num'])
-            }
-        }
-
-
-    return data
-
-def generate_alter_table_sql(action, data):
-    """
-    data = {
-        'table_name': sheet_name,
-        'new_col': {
-            'name': name,
-            'type': type
-        }
-        'remove_col': name,
-        'rename_col': {
-            'old_name': old_name,
-            'new_name': new_name,
-        }
-        'rename_table': {
-            'table': table_object,
-            'new_table_name': new_table_name
-        }
-    }
-    """
-    if action == 'add_col':
-        command = "ALTER TABLE {} ADD COLUMN {} {};".format(
-                data['table_name'],
-                data['new_col']['name'],
-                data['new_col']['type'])
-
-    elif action == 'remove_col':
-        command = "ALTER TABLE {} DROP COLUMN {};".format(
-                data['table_name'],
-                data['remove_col'])
-
-    elif action == 'rename_col':
-        command = "ALTER TABLE {} RENAME COLUMN {} TO {};".format(
-                data['table_name'],
-                data['rename_col']['old_name'],
-                data['rename_col']['new_name'])
-
-    elif action == 'rename_table':
-        pass
-    elif action == 'change_col_type':
-        pass
-    return command
-
 def user_adds_column(form, sheet, schema):
     new_col = models.Sheets_Schema(
             sheet, form.column_name.data,
@@ -134,7 +60,6 @@ def user_adds_column(form, sheet, schema):
     current_session = session.object_session(new_col)
     current_session.add(new_col)
     current_session.commit()
-    #return new_col
 
 def user_removes_columns(sheet, schema, request):
         cols_to_delete = request.form.getlist("to_delete")
@@ -143,19 +68,8 @@ def user_removes_columns(sheet, schema, request):
             col_to_delete = session.query(models.Sheets_Schema).filter(and_(
                 models.Sheets_Schema.column_name==name,
                 models.Sheets_Schema.sheet_id==sheet.id)).delete()
-          #  data = structure_alter_table_data('remove_col', col_to_delete)
-          #  commands.append(generate_alter_table_sql(
-          #      "remove_col", data))
-          #  session.delete(col_to_delete)
 
         leftover_columns = session.query(models.Sheets_Schema).filter(models.Sheets_Schema.sheet_id==sheet.id).all()
         for i, col in enumerate(leftover_columns):
-
             col.column_num = i
-            #data = structure_alter_table_data(
-            #    'rename_col', col, new_col_num=col.column_num - num_dropped)
-            #commands.append(generate_alter_table_sql(
-            #    "rename_col", data))
         session.commit()
-
-    #    return commands
