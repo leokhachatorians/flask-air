cb061b9d66290bc0708bc6ab9bf429f3098ffcfe
leokhachatorians <leokhachatorians@gmail.com>
2017-03-31 16:18:43 -0700

    displaying data

--- air/air.py
+++ air/air.py
@@ -6,6 +6,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import Table, MetaData
 import sqlalchemy
 
 app = Flask(__name__)
@@ -22,6 +23,7 @@ app.config.update(dict(
 app.config.from_envvar('AIR_SETTINGS', silent=True)
 
 db = SQLAlchemy(app)
+metadata = sqlalchemy.MetaData(bind=db.get_engine)
 Session = sessionmaker(bind=db.get_engine())
 #session = scoped_session(Session)
 session = Session()
@@ -61,12 +63,29 @@ def create_new_sheet():
         return redirect(url_for('create_new_sheet'))
     return render_template('create_new_sheet.html', form=form)
 
-@app.route('/view_sheet/<sheet_name>')
+@app.route('/view_sheet/<sheet_name>', methods=['GET', 'POST'])
 def view_sheet(sheet_name):
     sheet = session.query(models.Sheets).filter_by(sheet_name=sheet_name).first()
     schema = session.query(models.Sheets_Schema).filter(models.Sheets_Schema.sheet_id==sheet.id)
+    metadata = MetaData(bind=db.get_engine())
+    generated_table = Table("table_{}".format(
+        sheet.id), metadata, autoload=True)
+
+    if request.method == 'POST':
+        table_values = {}
+        for c, i in enumerate(request.form.getlist("add_records")):
+            table_values['col_{}'.format(c)] = i
+        engine = db.get_engine()
+        ins = generated_table.insert().values(table_values)
+        conn = engine.connect()
+        result = conn.execute(ins)
+        return redirect(url_for('view_sheet', sheet_name=sheet_name))
+
+    contents = session.query(generated_table).all()
+
     return render_template('view_sheet.html',
-            schema=schema, sheet_name=sheet_name)
+            schema=schema, sheet_name=sheet_name,
+            contents=contents)
 
 @app.route('/modify_sheet/<sheet_name>', methods=['GET', 'POST'])
 def modify_sheet(sheet_name):
@@ -91,7 +110,7 @@ def modify_sheet(sheet_name):
             helpers.user_removes_columns(sheet, schema, request)
             #print(data)
 
-        return redirect(url_for('view_sheet', sheet_name=sheet_name))
+        return redirect(url_for('modify_sheet', sheet_name=sheet_name))
     return render_template('modify_sheet.html',
             schema=schema, add_form=add_form,
             delete_form=delete_form, sheet_name=sheet_name)
