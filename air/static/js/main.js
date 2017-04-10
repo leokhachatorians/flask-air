d1187f830670d101ff2f8227cfba49d6050f872d
leo <leokhachatorians@gmail.com>
2017-04-10 16:49:08 -0700

    restrutured JS

--- air/static/js/main.js
+++ air/static/js/main.js
@@ -8,11 +8,12 @@ function toggle_well(_id) {
 	}
 }
 
-// Man I suck at Javascript.
-try {
-	var new_table_button = document.getElementById('new_table_button');
-	var cancel_table_button = document.getElementById('cancel_new_table_button');
+var new_table_button = document.getElementById('new_table_button');
+var cancel_table_button = document.getElementById('cancel_new_table_button');
+var new_column_button = document.getElementById('new_column_button');
+var cancel_column_button = document.getElementById('cancel_new_column_button');
 
+if (typeof(new_table_button) != 'undefined' && new_table_button != null) {
 	new_table_button.onclick = function() {
 		toggle_well('create_table_well');
 	};
@@ -20,10 +21,9 @@ try {
 	cancel_table_button.onclick = function() {
 		toggle_well('create_table_well');
 	};
-} catch (e) {
-	var new_column_button = document.getElementById('new_column_button');
-	var cancel_column_button = document.getElementById('cancel_new_column_button');
+}
 
+if (typeof(new_column_button) != 'undefined' && new_column_button != null) {
 	new_column_button.onclick = function() {
 		toggle_well('new_column_well');
 	};
