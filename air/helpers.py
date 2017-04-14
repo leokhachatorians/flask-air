5b282be4486c521403e243dc2b8b4253b0db9f79
leo <leokhachatorians@gmail.com>
2017-04-14 16:45:00 -0700

    added some comments here and there

--- air/helpers.py
+++ air/helpers.py
@@ -8,6 +8,13 @@ from wrappers import attempt_db_modification
 from migrate.changeset import *
 
 def generate_table(form, engine):
+    """Generates a table
+
+    Given a form object, get the name of the to be created table,
+    insert and commit it into our Sheets table. Afterwords, refresh
+    the metadata and use that to create a new table object which we
+    then just simply create.
+    """
     name = form.sheet_name.data
     sheet = models.Sheets(1, form.sheet_name.data)
     session.add(sheet)
@@ -70,31 +77,32 @@ def user_removes_columns(sheet, schema, request):
     session.commit()
 
 def user_alters_column(edit_form, sheet, request):
-    """
-    def upgrade(migrate_engine):
-        meta = MetaData(bind=migrate_engine)
-        users = Table('users', meta, autoload=True)
-        users.c.user_id.alter(name='id')
+    """Alters user picked columns
 
-    from sqlalchemy import update
-
-    stmt = update(users).where(users.c.id==5).\
-            values(name='user #5')
-
+    Currently just queries the database and alters the name
+    of the column. More work will be needed to properly adjust
+    column types (ensure data correction, leave as-is, delete, etc)
     """
     col_to_alter_id = request.form.getlist("col_to_alter")[0]
     col = session.query(models.Sheets_Schema).filter_by(id=col_to_alter_id).one()
     col.column_name = edit_form.column_name.data
-    session.commit()
-    #table = sqlalchemy.Table("table_{}".format(sheet.id), metadata, autoload=True)
 
 def user_deletes_table(sheets, request):
+    """Deletes a user generated tabled
+
+    Based on the id of the sheet, remove the corresponding generated table,
+    and the entry in 'Sheets' as well. Deletes the column definitions automatically.
+    """
     sheet_to_drop_id = request.form.getlist("sheet_to_drop")[0]
     generate_table = sqlalchemy.Table("table_{}".format(sheet_to_drop_id), metadata).drop()
     session.query(models.Sheets).filter_by(id=sheet_to_drop_id).delete()
     session.commit()
 
 def format_user_data(data):
+    """
+    Given various length tuples, trim the id and leave the rest
+    intact to display to the user.
+    """
     content = []
     for chunk in data:
         rows = []
